f65bb31464d206178b1f367c97738e44b7551544
tinhvd <dangtinha2@gmail.com>
2024-12-27 14:44:14 +0700

    bash and webhook of telegram

--- src/modules/telegram/telegram.module.ts
+++ src/modules/telegram/telegram.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import { TelegramService } from "./telegram.service";
 import { SettingModule } from "../../share_modules/setting/setting.module";
 import { TelegramController } from "./telegram.controller";
+import { LogModule } from "../../share_modules/log/log.module";
 
 @Module({
   imports:[
     SettingModule,
+    LogModule,
   ],
   providers: [TelegramService],
   controllers: [TelegramController],
