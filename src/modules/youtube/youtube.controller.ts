21581b81d5c9467fa8a571db440b25cde58465cb
dangtinhvu <dangtinha2@gmail.com>
2025-02-07 01:03:58 +0700

    add python preinstall

--- src/modules/youtube/youtube.controller.ts
+++ src/modules/youtube/youtube.controller.ts
@@ -44,6 +44,6 @@ export class YoutubeController {
 
   @Post('/music-trend')
   async musicTrend(@Body() data: any){
-    return this.service.musicNew(data['data']);
+    return this.service.musicNew(JSON.parse(data['data']));
   }
 }
